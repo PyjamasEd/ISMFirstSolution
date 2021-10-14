298dd584a12bac351ddf63063d0dd077efdc5e13
Edward <pyjamased@gmail.com>
2021-10-14 16:31:44 +0300

    Add Second Project

---
+++ ISMFirstSolution/ISMSecondConsoleProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISMSecondConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Second Project");
+        }
+    }
+}
